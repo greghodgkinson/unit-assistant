0f89fe6809f910c8b7b62ee9758fd01b57e3994c
greghodgkinson <39408336+greghodgkinson@users.noreply.github.com>
2025-08-25 07:10:58 +0200

    Add scrollbar to fullscreen editor

--- src/components/TiptapEditor.tsx
+++ src/components/TiptapEditor.tsx
@@ -415,7 +415,7 @@ export const TiptapEditor = forwardRef<TiptapEditorRef, TiptapEditorProps>(({
       </div>
 
       {/* Editor */}
-      <div className="flex-1 overflow-y-auto" style={{ minHeight: '200px' }}>
+      <div className="flex-1 overflow-y-auto max-h-full" style={{ minHeight: '200px' }}>
         <EditorContent editor={editor} />
       </div>
     </div>
